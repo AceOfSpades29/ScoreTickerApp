d75be3fc0cb0a953bbe8f65f80aeaa502c8a8c4f
swilliams29 <swillia47@rockets.utoledo.edu>
2020-03-29 14:18:16 -0500

    set option for using carousel view

--- ScoreTickerApp/ScoreTickerApp.Android/MainActivity.cs
+++ ScoreTickerApp/ScoreTickerApp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace ScoreTickerApp.Droid
 {
@@ -20,6 +21,8 @@ namespace ScoreTickerApp.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            //Needed for Carousel View
+            Forms.SetFlags("CarouselView_Experimental");
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
